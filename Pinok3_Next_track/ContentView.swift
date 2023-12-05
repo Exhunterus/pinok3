bb95eed06a911847dca06e37de29f00ea126e6d9
exhanter <2ivan.tkachev@gmail.com>
2023-12-05 09:31:32 +0100

    Initial Commit

---
+++ Pinok3_Next_track/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pinok3_Next_track
+//
+//  Created by Иван Ткачев on 05/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pinok3_Next_track/Pinok3_Next_trackApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pinok3_Next_trackApp.swift
+//  Pinok3_Next_track
+//
+//  Created by Иван Ткачев on 05/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Pinok3_Next_trackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
