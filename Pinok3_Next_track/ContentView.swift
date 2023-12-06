0485ca710af405f2672971c8cf26819571ab5ec0
exhanter <2ivan.tkachev@gmail.com>
2023-12-06 12:32:36 +0100

    Pinok3

--- Pinok3_Next_track/ContentView.swift
+++ Pinok3_Next_track/ContentView.swift
@@ -7,15 +7,45 @@
 
 import SwiftUI
 
+
+
+
+
+
+
+
 struct ContentView: View {
+
+    @State private var animationPlay2 = 1.0
+    @State private var animationPlay3 = 0.0
+    @State private var offset = 0.0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button {
+            withAnimation(.spring(bounce: 0.4).repeatCount(0), completionCriteria: .logicallyComplete) {
+                offset += 24
+                animationPlay2 -= 1
+                animationPlay3 += 1
+                        } completion: {
+                            offset = 0
+                            animationPlay3 = 0
+                            animationPlay2 = 1
+                        }
+            } label: {
+                HStack(spacing: 0) {
+                Image(systemName: "play.fill")
+                        .offset(x: 15)
+                        .offset(x: offset)
+                Image(systemName: "play.fill")
+                        .offset(x: 9)
+                        .scaleEffect(animationPlay2)
+                Image(systemName: "play.fill")
+                        .offset(x: 15)
+                        .scaleEffect(animationPlay3)
+                        .offset(x: -60)
+            }
         }
-        .padding()
+            .font(.largeTitle)
     }
 }
 
